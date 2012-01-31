92a51b4a73671321563a35a37dfff283d6275869
Sigurd Fosseng <sigurd@fosseng.net>
2012-01-31 01:11:29 +0100

    Added a simple motd server, UNTESTED

---
+++ src/q3/motd_server.py
@@ -0,0 +1,22 @@
+'''
+File: motd_server.py
+Author: Sigurd Fosseng
+Description: A simple MOTD server that displays a given text string
+
+RTCW motd port: 27951
+'''
+from protocol.q3 import Q3Protocol
+
+
+class Q3MotdServerProtocol(Q3Protocol):
+    def handle_motd(self, content, host, port):
+        content.strip("\"").lstrip("\"").lstrip("\\")
+        infodict = server_response_to_dict(content)
+        motd = "Hello World!"
+        returnstring = "motd \"challenge\\%s\\%s\\\"" % (infodict["challenge"], motd)
+
+        self.sendMessage(returnstring, (host, port))
+
+if __name__ == '__main__':
+    from twisted.internet import reactor
+    server = reactor.listenUDP(27951, Q3MotdServerProtocol())
